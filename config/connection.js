f2d1a23054aca4b8a3473c7249032fe7f2736292
Uyen Nguyen <udnguyen@ucsd.edu>
2020-12-31 16:23:12 -0800

    add jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
+var connection;
+if (process.env.JAWSDB)[
+  connection = mysql.createConnection(process.env.JAWSDB)
+]
+else {
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "Uyen3061",
   database: "burgers_db"
 });
+}
 
 connection.connect(function(err) {
   if (err) {
