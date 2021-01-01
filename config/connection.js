449a26d4175840d1660305d7b759e097e18645b8
Uyen Nguyen <udnguyen@ucsd.edu>
2020-12-31 16:56:49 -0800

    update jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 var mysql = require("mysql");
 var connection;
-if (process.env.JAWSDB)[
-  connection = mysql.createConnection(process.env.JAWSDB)
-]
-else {
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
   connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
